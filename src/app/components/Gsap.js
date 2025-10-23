3fd04282bc1312b2ee62cd523307ec33bb433f79
Mithran25 <navamithran25@gmail.com>
2025-10-23 20:21:31 +0530

    ninth commit

--- src/app/components/Gsap.js
+++ src/app/components/Gsap.js
@@ -1,42 +1,42 @@
 
-"use client"
+// "use client"
 
 
-import React from 'react'
-  import Image from "next/image";
-  import gym_1 from "@/assets/man-moving-giant-tire-wheel-gym.jpg";
-  import gym_2 from "@/assets/young-fitness-man-studio.jpg";
-  import gym_3 from "@/assets/fitness-rings-7530142_1280.jpg";
-  import Link from "next/link";
-  import { motion } from "framer-motion";
-  import ScrollCarousel from '@/components/lightswind/scroll-trigger-carousel.tsx';
+// import React from 'react'
+//   import Image from "next/image";
+//   import gym_1 from "@/assets/man-moving-giant-tire-wheel-gym.jpg";
+//   import gym_2 from "@/assets/young-fitness-man-studio.jpg";
+//   import gym_3 from "@/assets/fitness-rings-7530142_1280.jpg";
+//   import Link from "next/link";
+//   import { motion } from "framer-motion";
+//   import ScrollCarousel from '@/components/lightswind/scroll-trigger-carousel.tsx';
 
 
-  // const MotionImage = motion(Image);
+//   // const MotionImage = motion(Image);
 
-const Gsap = () => {
-    const features = [
-  {
-    image: "https://images.pexels.com/photos/9934462/pexels-photo-9934462.jpeg",
-  },
-  {
+// const Gsap = () => {
+//     const features = [
+//   {
+//     image: "https://images.pexels.com/photos/9934462/pexels-photo-9934462.jpeg",
+//   },
+//   {
 
-    title: "Discover class",
-    description: "Start your fitness journey with us.",
-    link :"Start"
-    // image: "https://images.pexels.com/photos/6988085/pexels-photo-6988085.jpeg",
-  },
+//     title: "Discover class",
+//     description: "Start your fitness journey with us.",
+//     link :"Start"
+//     // image: "https://images.pexels.com/photos/6988085/pexels-photo-6988085.jpeg",
+//   },
   
-];
-  return (
-    <div id="gsap">
-<ScrollCarousel
-  features={features}
-  className="my-20"
-  maxScrollHeight={2000}
-/>
-    </div>
-  )
-}
+// ];
+//   return (
+//     <div id="gsap">
+// <ScrollCarousel
+//   features={features}
+//   className="my-20"
+//   maxScrollHeight={2000}
+// />
+//     </div>
+//   )
+// }
 
-export default Gsap
+// export default Gsap
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Card from "@/app/components/Card";
 import Home from "@/app/components/Home";
 import WhyChoose from "@/app/components/WhyChoose"
 import Anima from "@/app/components/Anima"
-import Gsap from "@/app/components/Gsap"
+// import Gsap from "@/app/components/Gsap"
 
 export default function Page() {
 
