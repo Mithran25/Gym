a9ad09f772e5c80aec7a88bd578709a8279e68d5
Mithran25 <navamithran25@gmail.com>
2025-10-23 20:25:26 +0530

    ninth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Blog from "@/app/blog/page";
 import Card from "@/app/components/Card";
 import Home from "@/app/components/Home";
 import WhyChoose from "@/app/components/WhyChoose"
-import Anima from "@/app/components/Anima"
 // import Gsap from "@/app/components/Gsap"
 
 export default function Page() {
